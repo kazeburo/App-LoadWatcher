097e71578f2d544a810ee80a19605cb6f94e35fb
Masahiro Nagano <kazeburo@gmail.com>
2012-07-26 10:39:10 +0900

    first commit

--- lib/App/LoadWatcher.pm
+++ lib/App/LoadWatcher.pm
@@ -1,28 +1,30 @@
 package App::LoadWatcher;
+
 use strict;
 use warnings;
+use 5.008008;
+
 our $VERSION = '0.01';
 
 1;
+
 __END__
 
 =head1 NAME
 
-App::LoadWatcher -
+App::LoadWatcher - watch load average and execute a command
 
 =head1 SYNOPSIS
 
-  use App::LoadWatcher;
+ load_watcher [options] -- [command ...]
 
 =head1 DESCRIPTION
 
-App::LoadWatcher is
+See L<load_watcher>
 
 =head1 AUTHOR
 
 Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
-
-=head1 SEE ALSO
 
 =head1 LICENSE
 
